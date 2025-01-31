2f33d3022d4cf07a412faa8bc3ed53a707096c5f
helenaamado <helena.aamado@gmail.com>
2025-01-31 12:27:05 +0100

    added simple string

---
+++ slices_strings.py
@@ -0,0 +1,15 @@
+s= "abcdefghijklmnop"
+print(s)
+print(s[1:4], s[6:9])
+print(s[:4], s[6:])
+print(s[1:10:2]) #jumping every 2
+print(s[::-1])
+print("racecar"[::-1])
+s1="cat"
+s1 = "r" +s1[1:] #this is how you replace a letter
+print(s1)
+
+s2= "seven"
+s2 = s2[:2] +"7" +s2[3:]
+print(s2)
+
