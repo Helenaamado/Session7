00eec3f92bf0f4d5f577f712782cfc655efbd7b8
helenaamado <helena.aamado@gmail.com>
2025-01-31 11:24:04 +0100

    added simple string

---
+++ simple_string.py
@@ -0,0 +1,11 @@
+s= "hello"
+print(s) #this is how you define a string: s= "smt"
+s2= 'Hellow sir #@#@'
+print(s2)
+s3= ('and she said: "hello"')
+print(s3)
+print(",.'\"\"\"\"")
+s4= "hello!"
+print(s4[2], s4[4])# you index by position,  first index is 0
+print(s4[-1]) # go around the end
+print(s4[1], s4[-5])
